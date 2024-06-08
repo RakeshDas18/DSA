076135dc449f22724cf4a877edbb567ca9a2f885
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-08 11:48:32 +0530

    Add files via upload

---
+++ binarySearch.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main() {
+    int arr[100], i, n, item;
+    printf("Enter the number of elements: \n");
+    scanf("%d", &n);
+    printf("Write the value of the elements in order: \n");
+    for(i = 0; i < n; i++) {
+        printf("Enter the value of %d elements: \n", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the number you want to search: \n");
+    scanf("%d", &item);
+    int high = n - 1;
+    int low = 0;
+    int mid = (high + low) / 2;
+    while(low <= high){
+    	if(arr[mid] < item){
+    		low = mid + 1;
+		} else if (arr[mid] == item){
+			printf("%d is found at %d", item, mid + 1);
+			break;
+		} else {
+			high = mid - 1;
+		}
+		mid = (low+high)/2;
+	}
+	if(low > high){
+		printf("%d is not found!!!", item);
+	}
+    return 0;
+}
