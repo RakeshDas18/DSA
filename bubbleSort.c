dc2f433a26cf4e85704aaf66f135e48ee4033a35
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-09 17:55:52 +0530

    Update bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,27 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int arr[100], n, i, j, min, t;
+int main() {
+    int arr[100], n, i, j, temp;
     printf("Enter number of elements\n");
     scanf("%d", &n);
     printf("Enter %d integers\n", n);
     for (i=0;i<n;i++){
-        scanf("%d", &arr[i]);
-    }
+    	scanf("%d", &arr[i]);
+	}
+    
     for(i=0;i<(n-1);i++){
-        min = i;
-        for(j=i+1;j<n;j++){
-            if(arr[j] < arr[min]){
-                min = j; 
-            }
-        }
-        t = arr[min]; 
-        arr[min] = arr[i]; 
-        arr[i] = t;
-    }
+    	for(j=0;j<n-i-1;j++){
+    		if(arr[j]>arr[j+1]){
+    			temp = arr[j];
+    			arr[j] = arr[j+1];
+    			arr[j+1] = temp;
+			}
+		}
+	}
+    
     printf("Sorted list in ascending order:\n");
-    for(i=0;i<n;i++){
-        printf("%d\t", arr[i]);
-    }
+    for (i=0;i<n;i++){
+    	printf("%d\t", arr[i]);	
+	}
+
     return 0;
 }
