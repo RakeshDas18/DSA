55cd0178656aeaef0febd798af5a52cd4aa41bb7
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-06-10 07:07:22 +0530

    Update selectionSort.c

--- selectionSort.c
+++ selectionSort.c
@@ -1,28 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int array[100], n, i, j, min, t;
-    printf("Enter number of elements\n");
-    scanf("%d", &n);
-    printf("Enter %d integers\n", n);
-    for (i=0;i<n;i++)
-        scanf("%d", &array[i]);
-    for (i=0;i<(n-1);i++) {
-        min = i;
-        for (j=i+1;j<n;j++) {
-            if (array[min] > array[j])
-                min = j;
-        }
-        if (min != i) {
-            t = array[i];
-            array[i] = array[min];
-            array[min] = t;
-        }
-    }
-    printf("Sorted list in ascending order:\n");
-    for (i=0;i<n;i++)
-        printf("%d\t", array[i]);
-
-    return 0;
+int main(){
+	int arr[100], n, item, i, j, temp, min;
+	printf("Enter the total number of elements: \n");
+	scanf("%d", &n);
+	printf("Enter %d elements: \n", n);
+	for(i=0;i<n;i++){
+		scanf("%d", &arr[i]);
+	}
+	printf("Before sorting: \n");
+	for(i=0;i<n;i++){
+		printf("%d\t", arr[i]);
+	}
+	for(i=0;i<(n-1);i++){
+		min = i;
+		for(j=i+1;j<n;j++){
+			if(arr[min] > arr[j]){
+				min = j;
+			}
+		}
+		if(arr[min] != arr[i]){
+    		temp = arr[i];
+    		arr[i] = arr[min];
+    		arr[min] = temp;
+		}
+	}
+	printf("\nAfter sorting: \n");
+	for(i=0;i<n;i++){
+		printf("%d\t", arr[i]);
+	}
+	
+	
+	return 0;
 }
-
